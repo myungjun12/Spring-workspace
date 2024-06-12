a44b5c937b9a215499ed4752cbae8b3086104002
myungjun12 <nenncmj@naver.com>
2024-06-12 17:19:46 +0900

    thymeleaf

--- demoProject1/src/main/java/com/example/demo/controller/cafeController.java
+++ demoProject1/src/main/java/com/example/demo/controller/cafeController.java
@@ -3,7 +3,8 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.dto.CafeDTO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,12 +17,26 @@ import lombok.extern.slf4j.Slf4j;
 //<input type="text" name="cafeCommentOpinion" required>
 public class cafeController {
 	@PostMapping("comment")
-	public String cafeMainComment(@RequestParam("cafeCommentName") String cafeCommentName,
-							      @RequestParam("cafeCommentOpinion") String cafeCommentOpinion
-							) {
-		log.info("카페 메인으로 이동하기");
-		log.debug("cafeCommentName : " + cafeCommentName);
-		log.debug("cafeCommentOpinion" + cafeCommentOpinion);
+	public String cafeIndexComment(CafeDTO CafeDTO){
+		
+		CafeDTO cd = new CafeDTO();
+		
+		cd.getCafeCommentName();
+		cd.getCafeCommentOpinion();
+		log.info("cafeDTO getter : " + cd.toString());
+		
+		cd.setCafeCommentName("");
+		cd.setCafeCommentOpinion("");
+		log.info("cafeDTO setter : " + cd.toString());
 		return "redirect:/cafe/cafe-index";
 	}
+//	public String cafeMainComment(@RequestParam("cafeCommentName") String cafeCommentName,
+//							      @RequestParam("cafeCommentOpinion") String cafeCommentOpinion
+//							) {
+//		log.info("카페 메인으로 이동하기");
+//		log.debug("cafeCommentName : " + cafeCommentName);
+//		log.debug("cafeCommentOpinion" + cafeCommentOpinion);
+//		return "redirect:/cafe/cafe-index";
+//	}
+	
 }
---
+++ demoProject1/src/main/java/com/example/demo/model/dto/CafeDTO.java
@@ -0,0 +1,17 @@
+package com.example.demo.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
+public class CafeDTO {
+	private String cafeCommentName;
+	private String cafeCommentOpinion;
+}
