42e39f318308ac3e49464ceaa99d28bcd9084a79
myungjun12 <nenncmj@naver.com>
2024-06-19 15:32:58 +0900

    "Select"

---
+++ demoProject3-mybatis-mapper-insert-select1/src/main/java/com/khmall/DemoProject3MybatisMapperInsertSelect1Application.java
@@ -0,0 +1,13 @@
+package com.khmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProject3MybatisMapperInsertSelect1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProject3MybatisMapperInsertSelect1Application.class, args);
+	}
+
+}
