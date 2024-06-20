d52fa18b823e011e794bc8aebef54302a5eb5b82
myungjun12 <nenncmj@naver.com>
2024-06-20 14:27:15 +0900

    "image"

---
+++ demoProject3-mybatis-mapper-insert-select-join/src/main/java/com/khmall/DemoProject3MybatisMapperInsertSelectJoinApplication.java
@@ -0,0 +1,13 @@
+package com.khmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProject3MybatisMapperInsertSelectJoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProject3MybatisMapperInsertSelectJoinApplication.class, args);
+	}
+
+}
