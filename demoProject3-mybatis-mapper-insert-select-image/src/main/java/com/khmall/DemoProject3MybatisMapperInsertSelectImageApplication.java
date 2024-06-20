c2da47da408a58935805ace3a3cfa990c19ae863
myungjun12 <nenncmj@naver.com>
2024-06-20 14:29:28 +0900

    "image"

---
+++ demoProject3-mybatis-mapper-insert-select-image/src/main/java/com/khmall/DemoProject3MybatisMapperInsertSelectImageApplication.java
@@ -0,0 +1,13 @@
+package com.khmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProject3MybatisMapperInsertSelectImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProject3MybatisMapperInsertSelectImageApplication.class, args);
+	}
+
+}
